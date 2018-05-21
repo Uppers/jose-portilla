ed62949013229b11a78b36bdaf32bd8a9d3dee5c
Uppers <merchantfx@hotmail.com>
2018-05-21 05:46:04 +0100

    practicing loops in javascript

---
+++ javascript/section_one/loop_excersises.js
@@ -0,0 +1,27 @@
+var i=0
+
+while(i<5){
+    console.log("hello");
+    i++;
+}
+
+var x;
+
+for(x=0; x<5; x++){
+    console.log("hello");
+}
+
+var y=0
+
+while(y<=25){
+    if(y%2!=0){
+        console.log(y);
+    }
+    y++
+}
+
+for(y=0; y<=25; y++){
+    if(y%2!=0){
+        console.log(y)
+    }
+}
