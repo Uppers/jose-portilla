799eb95184f3b388290b88abe1a01b3efe30859b
Uppers <merchantfx@hotmail.com>
2018-05-21 04:09:07 +0100

    learning to generate alerts and print to browser

---
+++ javascript/section_one/lesson_one.js
@@ -0,0 +1,17 @@
+//Hello World!
+alert("Hello World!");
+console.log("Hello World!");
+console.log(1111);
+console.log("10");
+console.log(10);
+//exponents
+console.log(2**4);
+//Add Quotations
+console.log("\"Hello World!\"");
+console.log("'Hello World'");
+
+//Variables
+var deposit = 100;
+var approved_overdraft_limit = 100;
+var total_available = deposit+approved_overdraft_limit;
+console.log("amount available: "+total_available);
