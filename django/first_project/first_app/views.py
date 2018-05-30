10d2b8cff524981a24c7a7dc1b93ad7176787360
Uppers <merchantfx@hotmail.com>
2018-05-30 09:07:16 +0100

    adding templates file

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = {"insert_me": "<em>Hello I am from views.py!</em>"}
+    return render(request, 'first_app//index.html', context=my_dict)
 
 def fapp(request):
     return HttpResponse("<h1>fapping</h1>")
